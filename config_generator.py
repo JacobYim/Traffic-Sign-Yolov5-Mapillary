b42f251d1db76af1d81206644d265467e08f9dcc
JacobYim <dlagudah@gmail.com>
2022-01-18 13:23:10 +0900

    fix directory error

--- config_generator.py
+++ config_generator.py
@@ -2,7 +2,7 @@ from glob import glob
 import yaml
 from sklearn.model_selection import train_test_split
 
-img_list = glob('yolov5/yolo/yolo_data/*.jpg')
+img_list = list(map(lambda x : x[7:], glob('yolov5/yolo/yolo_data/*.jpg')))
 train_img_list, val_img_list = train_test_split(img_list, test_size=0.2, random_state=2000)
 
 with open('yolov5/yolo/train.txt', 'w') as f:
